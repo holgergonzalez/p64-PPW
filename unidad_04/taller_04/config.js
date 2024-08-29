bbb2dc3b9310ebc0fe98945f7730bb39ab186326
holgergonzalez <gonzalezh2002@gmail.com>
2024-08-29 10:38:24 -0500

    SSSs

--- unidad_04/taller_04/config.js
+++ unidad_04/taller_04/config.js
@@ -2,7 +2,7 @@ const config = {
     PORT: 3000,
     //DB_URL: 'mongodb://dba:dba@localhost:27020/miapp?authSource=admin'
     //DB_URL: 'mongodb://localhost:27017/''mongodb://localhost:27017/miapp?authSource=admin'
-    DB_URL: 'mongodb+srv://sulayvergara:STUV18ss@cluster0.0pmo6.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'
+    DB_URL: 'mongodb+srv://gonzalezh2002:<gonzalezh2002>@cluster0.kying.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'
 }
 
 module.exports = config
